10b09d3ae12a292a7eecbb3dc9f083525e4b5487
muzilka <norinessa2518@gmail.com>
2024-10-27 18:25:04 +0500

    Фикс бага

--- docs/stable/v1-0/scripts/c3runtime.js
+++ docs/stable/v1-0/scripts/c3runtime.js
@@ -4783,6 +4783,7 @@ self.C3_ExpressionFuncs = [
 			const n0 = p._GetNode(0);
 			return () => (n0.ExpObject() + 25);
 		},
+		() => 6,
 		p => {
 			const n0 = p._GetNode(0);
 			return () => (n0.ExpObject() + 1);
